d0a7d6a93c84a1e5924c8b714ff0320a8370b0bd
Md Rahi <fhrfhr102030@gmail.com>
2022-09-03 06:46:48 +0600

    add modal

--- js/portal.js
+++ js/portal.js
@@ -1,3 +1,6 @@
+document.getElementById('spinner').style.display = 'none';
+document.getElementById('footer').style.display = 'none';
+
 const loadCategories = async() => {
         const url = `https://openapi.programming-hero.com/api/news/categories`
         const res = await fetch(url);
@@ -17,14 +20,16 @@ const displayCategories = async(categoriesData) => {
 }
 
 const loadCategoryList = async(id) => {
+    document.getElementById('spinner').style.display = 'block';
+    document.getElementById('footer').style.display = 'block';
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data);
     displayCategoryList(data.data);
 }
 
 const displayCategoryList = async(data) =>{
+    document.getElementById('spinner').style.display = 'none';
     const categoryDetails = document.getElementById('category-details');
     categoryDetails.textContent = '';
         data.forEach(element => {
@@ -32,24 +37,24 @@ const displayCategoryList = async(data) =>{
             categoryDetailsDiv.classList.add('card');
             categoryDetailsDiv.innerHTML = `
             <div class="card lg:card-side bg-base-100 shadow-xl mb-4">
-                <figure><img src="${element.thumbnail_url}" alt="Album"></figure>
+                <figure><img src="${element.thumbnail_url ? element.thumbnail_url : "No data Found"}" alt="Album"></figure>
                 <div class="card-body">
-                    <h2 class="card-title">${element.title}</h2>
+                    <h2 class="card-title">${element.title ? element.title : "No data Found"}</h2>
                     <p>${element.details.length > 250 ? element.details.slice(0,250) + '...' : element.details}</p>
                     <div class="grid grid-cols-3 gap-4">
                         <div class="grid grid-cols-6 gap-4">
                             <div>
-                                <img class="rounded h-full w-12" src="${element.author.img}"/>
+                                <img class="rounded w-16" src="${element.author.img ? element.author.img : "No data Found"}"/>
                             </div>
                             <div>
-                                <p>${element.author.name}</p>
+                                <p>${element.author.name ? element.author.name : "No data Found"}</p>
                             </div>
                         </div>
                         <div>
-                            <i class="fa-solid fa-eye"> ${element.total_view}</i>
+                            <i class="fa-solid fa-eye"> ${element.total_view ? element.total_view : "No data Found"}</i>
                         </div>
-                        <div class="">
-                            <button class="btn btn-primary">More...</button>
+                        <div>
+                            <label for="my-modal-3" onclick="loadNews('${element._id}')" class="btn btn-primary modal-button">More...</label>
                         </div>
                     </div>
                 </div>
@@ -57,6 +62,28 @@ const displayCategoryList = async(data) =>{
             `;
             categoryDetails.appendChild(categoryDetailsDiv);
         });
+}
+
+const loadNews = async(newsId) => {
+    document.getElementById('spinner').style.display = 'block';
+    const url = `https://openapi.programming-hero.com/api/news/${newsId}`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayNewsDetails(data.data[0]);
+}
+
+const displayNewsDetails = async(info) => {
+    document.getElementById('spinner').style.display = 'none';
+    const newsDetails = document.getElementById('modal-body');
+        newsDetails.classList.remove('hidden');
+        newsDetails.textContent = '';
+        newsDetails.innerHTML =
+        `
+            <p class="py-4">${info.details}</p>
+            <h3 class="font-bold text-lg">Author: ${info.author.name}</h3>
+            <h3 class="font-bold text-lg">Published Date: ${info.author.published_date}</h3>
+            <h3 class="font-bold text-lg">Rating: ${info.rating.number}</h3>
+        `;
 }
 
 loadCategories();
